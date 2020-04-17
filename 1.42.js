8baa7f491a165b9a9636b95a691fd81ff291befc
Quentin Brogly <qbrogly@gmail.com>
2020-04-17 08:28:47 -0700

    Create 1.42.js

---
+++ 1.42.js
@@ -0,0 +1,7 @@
+// Let f and g be two one-argument functions. The composition f after g is defined to be the function x↦f(g(x)). 
+// Declare a function compose that implements composition. 
+// For example, if inc is a function that adds 1 to its argument: compose(square, inc)(6); returns 49.
+
+function compose(f, g){
+  return x => f(g(x))
+}
