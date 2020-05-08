020f8e32b000fc1f5a56dd9705f8f02129d5c63d
Quentin Brogly <qbrogly@gmail.com>
2020-05-08 08:46:19 -0700

    Create 2-25.js

---
+++ 2-25.js
@@ -0,0 +1,10 @@
+// Give combinations of heads and tails that will pick 7 from each of the following lists, given as printed by our interpreter:
+
+[1, [3, [[5, [7, null]], [9, null]]]]
+// head(tail(head(tail(tail(list)))));
+
+[[7, null], null]
+// head(head(list));
+
+[1,[[2,[[3,[[4,[[5,[[6,7,null]],null]],null]],null]],null]],null]]
+//head(tail(head(tail(head(tail(head(tail(head(tail(head(tail(list))))))))))));
