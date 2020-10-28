a929691d9a1e3a175700931724f6fde149d0bc3b
Quentin Brogly <qbrogly@gmail.com>
2020-10-28 11:06:43 -0700

    Update 3-23.js

--- 3-23.js
+++ 3-23.js
@@ -2,6 +2,14 @@ function make_deque() {
   let front_ptr = null;
   let rear_ptr = null;
 
+  function front_deque() {
+    return front_ptr;
+  }
+
+  function rear_deque() {
+    return rear_ptr;
+  }
+
   function make_node(item) {
     return pair(item, pair(null, null));
   }
@@ -36,8 +44,8 @@ function make_deque() {
       front_ptr = new_node;
       rear_ptr = new_node;
     } else {
-      set_prev_ptr(front_ptr, new_node);
       set_next_ptr(new_node, front_ptr);
+      set_prev_ptr(front_ptr, head(new_node));
       front_ptr = new_node;
     }
     return front_ptr;
@@ -49,9 +57,10 @@ function make_deque() {
       front_ptr = new_node;
       rear_ptr = new_node;
     } else {
-      set_prev_ptr(new_node, rear_ptr);
       set_next_ptr(rear_ptr, new_node);
+      const rear_val = node_value(rear_ptr);
       rear_ptr = new_node;
+      set_prev_ptr(rear_ptr, rear_val);
     }
     return rear_ptr;
   }
@@ -60,6 +69,8 @@ function make_deque() {
     if (is_empty_deque()) {
       error(front_ptr, "rear_delete_deque called with an empty deque");
     } else {
+      front_ptr = next_ptr(front_ptr);
+      set_prev_ptr(front_ptr, null);
       return front_ptr;
     }
   }
@@ -68,6 +79,8 @@ function make_deque() {
     if (is_empty_deque()) {
       error(rear_ptr, "rear_delete_deque called with an empty deque");
     } else {
+      rear_ptr = prev_ptr(rear_ptr);
+      set_next_ptr(rear_ptr, null);
       return rear_ptr;
     }
   }
