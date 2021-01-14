4716c318740bd293c239b72b09d29c1049a6ae14
Quentin Brogly <qbrogly@gmail.com>
2021-01-14 08:57:35 -0800

    Update 3-69.js

--- chapter-3/3-69.js
+++ chapter-3/3-69.js
@@ -6,3 +6,14 @@ function triples(i, j, k) {
     )
   );
 }
+
+function square(n) {
+    return n * n;
+}
+
+function is_pythagorean(x1, x2, x3) {
+    return square(x1) + square(x2) === square(x3);
+}
+
+const list_of_triples = triples(integers, integers, integers);
+const pythagorean_triples = stream_filter(list => is_pythagorean(head(list), head(tail(list)), head(tail(tail(list)))), list_of_triples);
