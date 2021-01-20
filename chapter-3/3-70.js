12723b16cbb6d830229ee3b30d1db5027a5d5c3a
Quentin Brogly <qbrogly@gmail.com>
2021-01-19 17:37:28 -0800

    Create 3-70.js

---
+++ chapter-3/3-70.js
@@ -0,0 +1,23 @@
+function weighted_pairs(s, t, w) {
+  return pair(list(head(s), head(t)), () =>
+    weighted_merge(
+      stream_map((x) => list(head(s), x), stream_tail(t)),
+      weighted_pairs(stream_tail(s), stream_tail(t), w),
+      w
+    )
+  );
+}
+
+function weighted_merge(s1, s2, w) {
+  if (is_null(s1)) {
+    return s2;
+  } else if (is_null(s2)) {
+    return s1;
+  } else {
+    const s1head = head(s1);
+    const s2head = head(s2);
+    return w(s1head) <= w(s2head)
+      ? pair(s1head, () => weighted_merge(stream_tail(s1), s2, w))
+      : pair(s2head, () => merge(s1, stream_tail(s2), w));
+  }
+}
